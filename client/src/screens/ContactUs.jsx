80a5f0bd2b1f2827dd60e81af5c925a78826845e
Zac Smart <zac_chaz@outlook.com>
2023-01-19 15:35:41 +1100

    working on contactus

--- client/src/screens/ContactUs.jsx
+++ client/src/screens/ContactUs.jsx
@@ -42,112 +42,112 @@
 
 
 import React, { useState } from 'react';
-import {validateEmail } from '../../Utils/helpers';
+import { validateEmail } from '../../src/util/helpers';
 
 function ContactUs() {
-  
-  const [email, setEmail] = useState('');
-  const [sender, setSender] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  
 
-  const handleInputChange = (e) => {
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-    console.log({inputType, inputValue});
+    const [email, setEmail] = useState('');
+    const [sender, setSender] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    
-    if (inputType === 'sender') {
-      setSender(inputValue);
-    } else if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'message') {
-      setMessage(inputValue);
-    }
-  };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if (!validateEmail(email) || !sender) {
-      setErrorMessage('Email or name is invalid');
-      return;
-    }
-    setSender('');
-    setEmail('');
-    setMessage('');
-    setErrorMessage('');
-    alert(`Cheers ${sender}, thanks for the message.`)
-  };
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+        console.log({ inputType, inputValue });
 
-  return (
-    <section className='container-fluid contact'>
-      <h2 id="contact-title">Contact Me</h2>
-      <div className='row contact-row' data-contact="hide">
-        <form
-        action='mailto:'
-        method='POST'
-        encType='multipart/form-data'
-        id='contact-form'
-        >
-          <div className='form-group'>
-            <label htmlFor='name'>Name</label>
-            <input
-            value={sender}
-            type="text"
-            className='form-control'
-            name="name"
-            onChange={handleInputChange}
-            id="full-name"
-            placeholder='Name'
-            required
-            />
-            
+
+        if (inputType === 'sender') {
+            setSender(inputValue);
+        } else if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'message') {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        if (!validateEmail(email) || !sender) {
+            setErrorMessage('Email or name is invalid');
+            return;
+        }
+        setSender('');
+        setEmail('');
+        setMessage('');
+        setErrorMessage('');
+        alert(`Cheers ${sender}, thanks for the message.`);
+    };
+
+    return (
+        <section className='container-fluid contact'>
+            <h2 id="contact-title">Contact Us</h2>
+            <div className='row contact-row' data-contact="hide">
+                <form
+                    action='mailto:'
+                    method='POST'
+                    encType='multipart/form-data'
+                    id='contact-form'
+                >
+                    <div className='form-group'>
+                        <label htmlFor='name'>Name</label>
+                        <input
+                            value={sender}
+                            type="text"
+                            className='form-control'
+                            name="name"
+                            onChange={handleInputChange}
+                            id="full-name"
+                            placeholder='Name'
+                            required
+                        />
+
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            value={email}
+                            type="email"
+                            className='form-control'
+                            name='email'
+                            onChange={handleInputChange}
+                            id="email"
+                            placeholder='Email'
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="message">Message:</label>
+                        <textarea
+                            value={message}
+                            className='form-control'
+                            name='message'
+                            onChange={handleInputChange}
+                            id="message"
+                            placeholder='Please Send Me a Message'
+                            rows="4"
+                            cols=""
+                            required
+                        ></textarea>
+                    </div>
+                    <button type='submit'
+                        id="email-btn"
+                        className='subBtn'
+                        onClick={handleFormSubmit}
+                    >submit</button>
+                </form>
+                {errorMessage && (
+                    <div>
+                        <p className='error-text'>{errorMessage}</p>
+                    </div>
+                )}
             </div>
-            <div className='form-group'>
-              <label htmlFor="email">Email:</label>
-              <input
-              value={email}
-              type="email"
-              className='form-control'
-              name='email'
-              onChange={handleInputChange}
-              id="email"
-              placeholder='Email'
-              required
-              />
-          </div>
-          <div className='form-group'>
-          <label htmlFor="message">Message:</label>
-              <textarea
-              value={message}
-              className='form-control'
-              name='message'
-              onChange={handleInputChange}
-              id="message"
-              placeholder='Please Send Me a Message'
-              rows="4"
-              cols=""
-              required
-              ></textarea>
-          </div>
-          <button type='submit'
-           id="email-btn"
-           className='subBtn'
-           onClick={handleFormSubmit}
-           >submit</button>
-        </form>
-        {errorMessage && (
-          <div>
-            <p className='error-text'>{errorMessage}</p>
-            </div>
-        )}
-      </div>
-      </section>
-  )
-   
-  
+        </section>
+    );
+
+
 }
 
 export default ContactUs;
